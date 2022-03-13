1dfa944631ae91d096e8edc926fa85f07db669db
April <1661154048@qq.com>
2022-03-13 16:02:49 +0800

    完成商品功能开发

--- src/main.js
+++ src/main.js
@@ -8,7 +8,17 @@ import './assets/fonts/iconfont.css'
 import './assets/css/global.css'
 import TreeTable from 'vue-table-with-tree-grid'
 
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// 导入富文本对应的样式
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
+
+
+
 import axios from 'axios'
+
 // 配置请求根路径
 axios.defaults.baseURL = 'https://lianghj.top:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
@@ -22,6 +32,23 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 Vue.component('tree-table', TreeTable)
+    // 将富文本编辑器注册为全局可用的组件
+Vue.use(VueQuillEditor)
+
+// 全局的时间过滤器
+Vue.filter('dateFormat', function(originVal) {
+    const dt = new Date(originVal * 1000)
+
+    const y = dt.getFullYear()
+        // padStart指要满足多少位，不足多少位用什么补
+    const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+    const d = (dt.getDay() + '').padStart(2, '0')
+    const hh = (dt.getHours() + '').padStart(2, '0')
+    const mm = (dt.getMinutes() + '').padStart(2, '0')
+    const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+    return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
     router,
